0aa0e01a6aac37890d15689391d84439ed93091a
mithamk <mithamk2004@gmail.com>
2026-04-17 14:53:04 +0530

    Add files via upload

--- src/main/java/com/hospito/service/DriverService.java
+++ src/main/java/com/hospito/service/DriverService.java
@@ -1,3 +1,23 @@
+/*
+ * Author: Mitha M K
+ * Class: Sem 6 Sec F
+ * Project: Hospito
+ * 
+ * Author: N Rakshitha
+ * Class: Sem 6 Sec F
+ * Project: Hospito
+*/
+
+/*
+Design Principles Used:
+1. Separation of Concerns (SoC)
+2. Repository Pattern
+3. Abstraction
+4. Dependency Injection (DI) (used by Spring Data internally)
+5. Convention over Configuration
+6. Single Responsibility Principle (SRP)
+*/
+
 package com.hospito.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
