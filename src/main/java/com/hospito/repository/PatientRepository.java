801e36d38324615aaf7203ed71ff0970a0446ab4
Rakshitha-Raksta <raksta777@gmail.com>
2026-04-17 14:51:54 +0530

    Add files via upload

--- src/main/java/com/hospito/repository/PatientRepository.java
+++ src/main/java/com/hospito/repository/PatientRepository.java
@@ -1,3 +1,19 @@
+/*
+ * Author: N Rakshitha
+ * Class: Sem 6 Sec F
+ * Project: Hospito
+*/
+
+/*
+Design Principles Used:
+1. Separation of Concerns (SoC)
+2. Repository Pattern
+3. Abstraction
+4. Dependency Injection (DI) (used by Spring Data internally)
+5. Convention over Configuration
+6. Single Responsibility Principle (SRP)
+*/
+
 package com.hospito.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
